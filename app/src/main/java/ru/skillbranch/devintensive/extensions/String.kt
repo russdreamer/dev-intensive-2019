915cd27ae2559037c5db59ae417c827a14bf2d46
Igor K <russdreamer@gmail.com>
2019-07-04 01:36:20 +0200

    # truncate() now return full string if endIndex is big enough # fix and improve stripHtml() to parse any html escape characters

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,12 +2,11 @@ package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(endIndex: Int = 16): String{
     val trimmedMes = this.trim()
-    val subLine = trimmedMes.substring(0, endIndex + 1).trim()
-    return if (subLine.length < trimmedMes.length) "$subLine..." else subLine
+    return if (trimmedMes.length <= endIndex + 1) trimmedMes else trimmedMes.substring(0, endIndex + 1).trim() + "..."
 }
 
 fun String.stripHtml(): String{
-    val htmlRegex = Regex("<.*?>|&amp;|&lt;|&gt;|&#39;|&quot;|&apos;")
-    val spaceRegex = Regex("\\s{2,}")
+    val htmlRegex = Regex("(<.*?>)|(&[^ а-я]{1,4}?;)")
+    val spaceRegex = Regex(" {2,}")
     return this.replace(htmlRegex, "").replace(spaceRegex, " ")
 }
