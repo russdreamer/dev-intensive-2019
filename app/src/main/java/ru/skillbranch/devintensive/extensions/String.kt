d01ce9f46818af15c5b0162ccfa01a4e010f98a3
Igor K <russdreamer@gmail.com>
2019-07-03 18:29:38 +0200

    html striping method

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,6 +1,13 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(endIndex: Int = 16): String{
-    val subLine = this.substring(0, endIndex + 1).trim()
-    return if (subLine.length < this.trim().length) "$subLine..." else subLine
+    val trimmedMes = this.trim()
+    val subLine = trimmedMes.substring(0, endIndex + 1).trim()
+    return if (subLine.length < trimmedMes.length) "$subLine..." else subLine
+}
+
+fun String.stripHtml(): String{
+    val htmlRegex = Regex("<.*?>|&amp;|&lt;|&gt;|&#39;|&quot;|&apos;")
+    val spaceRegex = Regex("\\s{2,}")
+    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
 }
