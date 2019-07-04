65f94c0f6eaa64604ed464224d3426afbca869a4
Igor K <russdreamer@gmail.com>
2019-07-04 02:34:47 +0200

    made parsing name a bit shorter

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,9 +4,9 @@ import java.lang.StringBuilder
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?>{
-        val parts = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0).orEmpty().trim().ifEmpty { null }
-        val lastName = parts?.getOrNull(1).orEmpty().trim().ifEmpty { null }
+        val parts = fullName?.trim()?.split(" ")
+        val firstName = parts?.getOrNull(0)?.ifEmpty { null }
+        val lastName = parts?.getOrNull(1)?.ifEmpty { null }
 
         return firstName to lastName
     }
