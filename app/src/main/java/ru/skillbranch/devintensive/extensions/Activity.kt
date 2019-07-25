bda30b4e06a8ecc467eeea3e1e83f45118a8a78f
Igor K <russdreamer@gmail.com>
2019-07-25 12:56:57 +0200

    restore project structure

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,36 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToLong
+
+fun Activity.hideKeyboard(){
+    val focus = this.currentFocus
+    focus?.let {
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.let {
+            it.hideSoftInputFromWindow(focus.windowToken, 0)
+        }
+    }
+}
+
+fun Activity.isKeyboardOpen(): Boolean{
+    val rootView = findViewById<View>(android.R.id.content)
+    val visibleBounds = Rect()
+    rootView.getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = rootView.height - visibleBounds.height()
+    val marginOfError = convertDpToPx(this, 50F).roundToLong()
+
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return this.isKeyboardOpen().not()
+}
+
+fun convertDpToPx(activity: Activity, dp: Float): Float {
+    return TypedValue.applyDimension (TypedValue.COMPLEX_UNIT_DIP, dp, activity.resources.displayMetrics)
+}
