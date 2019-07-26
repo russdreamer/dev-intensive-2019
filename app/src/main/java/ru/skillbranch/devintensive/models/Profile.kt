bae48c694431834880a7214336ac015867dea958
Igor K <russdreamer@gmail.com>
2019-07-26 03:38:42 +0200

    compute nickname based on fist and last names via transliteration

--- app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.utils.Utils
+
 data class Profile (
     val firstName: String,
     val lastName: String,
@@ -8,7 +10,7 @@ data class Profile (
     val rating: Int = 0,
     val respect: Int = 0
 ) {
-    val nickName: String = "John Doe" //TODO
+    val nickName: String = Utils.transliteration("$firstName $lastName", "_")
     val rank: String = "Junior Android Developer"
 
     fun toMap(): Map<String, Any> = mapOf(
