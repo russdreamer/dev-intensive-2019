9b420d97cb8802cb1e592fb90209df2b487ece57
Igor K <russdreamer@gmail.com>
2019-09-25 19:15:48 +0300

    left the fastest draw method only

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -3,11 +3,9 @@ package ru.skillbranch.devintensive.ui.custom
 import android.content.Context
 import android.graphics.*
 import android.graphics.Bitmap.Config
-import android.graphics.Bitmap.createBitmap
 import android.graphics.PorterDuff.Mode
 import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.annotation.ColorInt
@@ -19,8 +17,6 @@ import androidx.core.graphics.drawable.toBitmap
 import ru.skillbranch.devintensive.App
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.utils.Utils
-import java.util.*
-import kotlin.math.max
 import kotlin.math.min
 
 
@@ -32,9 +28,10 @@ class CircleImageView @JvmOverloads constructor (context: Context, attrs: Attrib
 
     private var borderColor = DEFAULT_BORDER_COLOR
     private var borderWidth = Utils.convertDpToPx(context, 2F)
-    private val path = Path()
     private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val srcMode = PorterDuffXfermode(Mode.SRC)
+    private val srcInMode = PorterDuffXfermode(Mode.SRC_IN)
 
     init {
         if (attrs != null) {
@@ -69,72 +66,7 @@ class CircleImageView @JvmOverloads constructor (context: Context, attrs: Attrib
         this.invalidate()
     }
 
-    /* ЗАМЕРЫ ПРОИЗВОДЯТСЯ ЗДЕСЬ */
     override fun onDraw(canvas: Canvas) {
-        val start = System.currentTimeMillis()
-        repeat(1_000) {pathMethodDraw(canvas)}
-        val end = System.currentTimeMillis()
-        Log.d("M_CircleImageView", "method spent ${end - start} ms")
-    }
-
-    /*отрисовка битмапа - 9000, сдвиг 40_000, */
-    private fun pathMethodDraw(canvas: Canvas) {
-        if (width == 0 || height == 0) return
-        getBitmapFromDrawable(width, height) ?: return
-
-        val halfWidth = width / 2F
-        val halfHeight = height / 2F
-        val radius = min(halfWidth, halfHeight)
-        path.addCircle(halfWidth, halfHeight, radius, Path.Direction.CCW)
-        canvas.clipPath(path)
-
-        super.onDraw(canvas)
-
-        if (borderWidth > 0) {
-            borderPaint.color = borderColor
-            borderPaint.strokeWidth = borderWidth.toFloat()
-            canvas.drawCircle(halfWidth, halfHeight, radius - borderWidth / 2, borderPaint)
-        }
-    }
-
-    /*отрисовка битмапа - 1500, сдвиг 850, */
-    private fun defaultMethodDraw(canvas: Canvas) {
-        var bitmap = getBitmapFromDrawable(width, height) ?: return
-        if (width == 0 || height == 0) return
-
-        bitmap = getScaledBitmap(bitmap, width)
-        bitmap = getCenterCroppedBitmap(bitmap, width)
-        bitmap = getCircleBitmap(bitmap)
-
-        if (borderWidth > 0)
-            bitmap = getStrokedBitmap(bitmap, borderWidth, borderColor)
-
-        canvas.drawBitmap(bitmap, 0F, 0F, null)
-    }
-
-    /*отрисовка битмапа - 800, сдвиг 1200 */
-    private fun shaderMethodDraw(canvas: Canvas) {
-        val bitmap = getBitmapFromDrawable(width, height) ?: return
-        if (width == 0 || height == 0) return
-
-        val halfWidth = width / 2F
-        val halfHeight = height / 2F
-        val radius = min(halfWidth, halfHeight)
-
-        val shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-        circlePaint.shader = shader
-
-        canvas.drawCircle(halfWidth, halfHeight, radius, circlePaint)
-
-        if (borderWidth > 0) {
-            borderPaint.color = borderColor
-            borderPaint.strokeWidth = borderWidth.toFloat()
-            canvas.drawCircle(halfWidth, halfHeight, radius - borderWidth / 2, borderPaint)
-        }
-    }
-
-    /* отрисовка битмапа - 600, сдвиг 0 */
-    private fun improvedDefaultMethodDraw(canvas: Canvas) {
         if (width == 0 || height == 0) return
         val bitmap = getBitmapFromDrawable(width, height) ?: return
 
@@ -142,9 +74,9 @@ class CircleImageView @JvmOverloads constructor (context: Context, attrs: Attrib
         val halfHeight = height / 2F
         val radius = min(halfWidth, halfHeight)
 
-        circlePaint.xfermode = PorterDuffXfermode(Mode.SRC)
+        circlePaint.xfermode = srcMode
         canvas.drawCircle(halfWidth, halfHeight, radius, circlePaint)
-        circlePaint.xfermode = PorterDuffXfermode(Mode.SRC_IN)
+        circlePaint.xfermode = srcInMode
         canvas.drawBitmap(bitmap, 0F, 0F, circlePaint)
 
         if (borderWidth > 0) {
@@ -152,60 +84,6 @@ class CircleImageView @JvmOverloads constructor (context: Context, attrs: Attrib
             borderPaint.strokeWidth = borderWidth.toFloat()
             canvas.drawCircle(halfWidth, halfHeight, radius - borderWidth / 2, borderPaint)
         }
-    }
-
-    /* отрисовка битмапа - 24000, сдвиг - не долждался */
-    private fun pathPaintMethodDraw(canvas: Canvas) {
-        if (width == 0 || height == 0) return
-        getBitmapFromDrawable(width, height) ?: return
-
-        val halfWidth = width / 2F
-        val halfHeight = height / 2F
-        val radius = min(halfWidth, halfHeight)
-
-        super.onDraw(canvas)
-        path.addCircle(halfWidth, halfHeight, radius, Path.Direction.CCW)
-        circlePaint.xfermode = PorterDuffXfermode(Mode.DST_IN)
-        canvas.drawPath(path, circlePaint)
-
-        if (borderWidth > 0) {
-            borderPaint.color = borderColor
-            borderPaint.strokeWidth = borderWidth.toFloat()
-            canvas.drawCircle(halfWidth, halfHeight, radius - borderWidth / 2, borderPaint)
-        }
-    }
-
-    private fun getStrokedBitmap(squareBmp: Bitmap, strokeWidth: Int, color: Int): Bitmap {
-        val inCircle = RectF()
-        val strokeStart = strokeWidth / 2F
-        val strokeEnd = squareBmp.width - strokeWidth / 2F
-
-        inCircle.set(strokeStart , strokeStart, strokeEnd, strokeEnd)
-
-        val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        strokePaint.color = color
-        strokePaint.style = Paint.Style.STROKE
-        strokePaint.strokeWidth = strokeWidth.toFloat()
-
-        val canvas = Canvas(squareBmp)
-        canvas.drawOval(inCircle, strokePaint)
-
-        return squareBmp
-    }
-
-    private fun getCenterCroppedBitmap(bitmap: Bitmap, size: Int): Bitmap {
-        val cropStartX = (bitmap.width - size) / 2
-        val cropStartY = (bitmap.height - size) / 2
-
-        return Bitmap.createBitmap(bitmap, cropStartX, cropStartY, size, size)
-    }
-
-    private fun getScaledBitmap(bitmap: Bitmap, minSide: Int) : Bitmap {
-        return if (bitmap.width != minSide || bitmap.height != minSide) {
-            val smallest = min(bitmap.width, bitmap.height).toFloat()
-            val factor = smallest / minSide
-            Bitmap.createScaledBitmap(bitmap, (bitmap.width / factor).toInt(), (bitmap.height / factor).toInt(), false)
-        } else bitmap
     }
 
     private fun getBitmapFromDrawable(width: Int, height: Int): Bitmap? {
@@ -220,23 +98,6 @@ class CircleImageView @JvmOverloads constructor (context: Context, attrs: Attrib
         drawable.setBounds(0, 0, canvas.width, canvas.height)
         drawable.draw(canvas)
         return bmp
-    }
-
-    private fun getCircleBitmap(bitmap: Bitmap): Bitmap {
-        val smallest = min(bitmap.width, bitmap.height)
-        val outputBmp = Bitmap.createBitmap(smallest, smallest, Config.ARGB_8888)
-        val canvas = Canvas(outputBmp)
-
-        val paint = Paint()
-
-        paint.isAntiAlias = true
-        paint.isFilterBitmap = true
-        paint.isDither = true
-        canvas.drawCircle(smallest / 2F, smallest / 2F, smallest / 2F, paint)
-        paint.xfermode = PorterDuffXfermode(Mode.SRC_IN)
-        canvas.drawBitmap(bitmap, 0F, 0F,  paint)
-
-        return outputBmp
     }
 }
 
